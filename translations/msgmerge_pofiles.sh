5de8f6de7f0d83d3a2ee802ea4258e0298e0e3b0
Eric Gallager <egall@gwmail.gwu.edu>
2023-02-28 21:50:39 -0500

    a few further refinements

--- translations/msgmerge_pofiles.sh
+++ translations/msgmerge_pofiles.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 for lang in $(cat LINGUAS); do msgmerge --previous --update --lang=${lang} ${lang}.po wesnoth-sotbeee.pot; done
+diff -u wesnoth-sotbe.pot wesnoth-sotbeee.pot > potfile.diff
