c2658768ce2705f98fe7e3f605f75b45374555af
Eric Gallager <egall@gwmail.gwu.edu>
2024-10-27 02:03:54 -0400

    yamllint

--- translations/msgmerge_pofiles.sh
+++ translations/msgmerge_pofiles.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # The command to update the potfile is usually something like:
-# ./wmlxgettext --domain=wesnoth-sotbeee --recursive --warnall --no-sort-by-file --directory=/Users/ericgallager/Library/Containers/org.wesnoth.Wesnoth/Data/Library/Application\ Support/Wesnoth_1.16/data/add-ons/Son_Of_The_Black_Eye_Easy_Edition -o /Users/ericgallager/Library/Containers/org.wesnoth.Wesnoth/Data/Library/Application\ Support/Wesnoth_1.16/data/add-ons/Son_Of_The_Black_Eye_Easy_Edition/translations/wesnoth-sotbeee.pot
+# ./wmlxgettext --domain=wesnoth-sotbeee --recursive --warnall --no-sort-by-file --directory=/Users/ericgallager/Library/Containers/org.wesnoth.Wesnoth/Data/Library/Application\ Support/Wesnoth_1.18/data/add-ons/Son_Of_The_Black_Eye_Easy_Edition -o /Users/ericgallager/Library/Containers/org.wesnoth.Wesnoth/Data/Library/Application\ Support/Wesnoth_1.18/data/add-ons/Son_Of_The_Black_Eye_Easy_Edition/translations/wesnoth-sotbeee.pot
 # (with edits as appropriate to reflect the proper paths)
 
 for lang in $(cat LINGUAS); do msgmerge --previous --update --lang=${lang} ${lang}.po wesnoth-sotbeee.pot; done
