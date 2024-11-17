4805575b933222392f6a5c7d3928f67d68942e1a
Eric Gallager <egall@gwmail.gwu.edu>
2024-11-17 12:18:27 -0500

    update translation merge script

--- translations/msgmerge_pofiles.sh
+++ translations/msgmerge_pofiles.sh
@@ -4,5 +4,8 @@
 # ./wmlxgettext --domain=wesnoth-sotbeee --recursive --warnall --no-sort-by-file --directory=/Users/ericgallager/Library/Containers/org.wesnoth.Wesnoth/Data/Library/Application\ Support/Wesnoth_1.18/data/add-ons/Son_Of_The_Black_Eye_Easy_Edition -o /Users/ericgallager/Library/Containers/org.wesnoth.Wesnoth/Data/Library/Application\ Support/Wesnoth_1.18/data/add-ons/Son_Of_The_Black_Eye_Easy_Edition/translations/wesnoth-sotbeee.pot
 # (with edits as appropriate to reflect the proper paths)
 
-for lang in $(cat LINGUAS); do msgmerge --previous --update --lang=${lang} ${lang}.po wesnoth-sotbeee.pot; done
+for lang in $(cat LINGUAS); do
+  echo "merging translation for ${lang} with template..."
+  msgmerge --previous --update --lang=${lang} ${lang}.po wesnoth-sotbeee.pot
+done
 diff -u wesnoth-sotbe.pot wesnoth-sotbeee.pot > potfile.diff
